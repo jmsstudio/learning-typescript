7c03c69078e151a91f699050d35ec1f5c67c1ff7
Jefferson M Souza <jefferson.msouza86@gmail.com>
2018-09-09 00:12:57 -0300

    Getting started with typescript

---
+++ app/ts/app.ts
@@ -0,0 +1,2 @@
+const negotiation = new Negotiation(new Date(), 12, 100);
+console.log(negotiation.value);
---
+++ app/ts/models/Negotiation.ts
@@ -0,0 +1,24 @@
+class Negotiation {
+  private _date;
+  private _amount;
+  private _value;
+
+  constructor(date, amount, value) {
+    this._date = date;
+    this._amount = amount;
+    this._value = value;
+  }
+
+  get date() {
+    return this._date;
+  }
+  get amount() {
+    return this._amount;
+  }
+  get value() {
+    return this._value;
+  }
+  get volume() {
+    return this._value * this._amount;
+  }
+}
