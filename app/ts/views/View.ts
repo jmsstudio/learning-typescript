d70cb124004943d875205315b9eebb4a951e4503
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2018-09-23 22:32:44 -0300

    using abstract class and method

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,15 +1,13 @@
-class View<T> {
-  protected _element: Element;
+abstract class View<T> {
+  private _element: Element;
 
   constructor(elementSelector: string) {
     this._element = document.querySelector(elementSelector);
   }
 
-  template(negotiations: T): string {
-    throw new Error('You need to implement the method template');
-  }
-
   update(model: T): void {
     this._element.innerHTML = this.template(model);
   }
+
+  abstract template(negotiations: T): string;
 }
